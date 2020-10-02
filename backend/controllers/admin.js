94244a2a355286148e1ef81470e59d04ca16b055
Tom Martin Roberg <tom.martin.roberg@knowit.no>
2020-10-02 19:10:33 +0200

    intial setup of the MERN Stack

---
+++ backend/controllers/admin.js
@@ -0,0 +1,38 @@
+const Anime = require('../models/Anime');
+
+exports.getIndex = async (req, res) => {
+    const anime = await Anime.find((data) => data);
+
+    try {
+        console.log(anime);
+        res.status(200).render('index', { anime: anime });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+exports.getAnime = async (req, res) => {
+    const animeId = req.params.animeId;
+
+    const anime = await Anime.findById(animeId, (anime) => anime);
+
+    try {
+        console.log(anime);
+        res.status(200).render('anime', { anime: anime });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+exports.getAddAnime = (req, res) => {
+    res.status(200).render('edit-anime');
+};
+
+exports.postAnime = (req, res) => {
+    const { name, image, description } = req.body;
+
+    const anime = new Anime({ name: name, image: image, description: description });
+    anime.save();
+    console.log('Anime Added to the database');
+    res.status(201).redirect('/');
+};
